1c46bdeda606337a4d3588277bf7a3e58dac3260
Jake Kidd <jcb.kdd@gmail.com>
2024-01-22 13:11:48 -0800

    debug test erc20 deploy

--- src/stateChannelTest.ts
+++ src/stateChannelTest.ts
@@ -5,7 +5,7 @@ import { Wallet, ContractFactory } from "ethers";
 import { ChannelManager } from "./ChannelManager";
 import * as dotenv from "dotenv";
 
-dotenv.config({ path: "../.local.env" });
+dotenv.config();
 
 // TODO: Move to tests dir.
 
@@ -13,18 +13,18 @@ async function main() {
   // Using Sepolia as the test chain.
   const chainId = 111555111;
 
-  // const sugarDaddy = new Wallet(process.env.MNEMONIC);
-  console.log("mn", process.env["MNEMONIC"]);
+  const sugarDaddy = Wallet.fromPhrase(process.env.MNEMONIC);
 
   // Get TEST token contract. If there is no contract, deploy one.
-  // const factory = new ContractFactory(TestERC20.abi, TestERC20.bytecode);
-  // const deployArgs = ["Test", "TEST"];
-  // factory.connect(sugarDaddy);
-  // const contract = await factory.deploy(deployArgs);
+  const factory = new ContractFactory(TestERC20.abi, TestERC20.bytecode);
+  const deployArgs = ["Test", "TEST"];
+  factory.connect(sugarDaddy);
+  const contract = await factory.deploy(deployArgs);
+  const dpTx = await contract.deploymentTransaction().wait();
 
-  // console.log(contract.getAddress());
-  // console.log(contract.deploymentTransaction());
-  // console.log("hello world");
+  console.log(contract.getAddress());
+  console.log(dpTx);
+  console.log(contract.deploymentTransaction())
 }
 
 main();
