0bc053905a2cb01342f50b255c757e6b73a6cbd7
Jake Kidd <jcb.kdd@gmail.com>
2024-01-22 21:50:14 -0800

    debug signing

--- src/stateChannelTest.ts
+++ src/stateChannelTest.ts
@@ -1,7 +1,15 @@
 import Payments from "../artifacts/contracts/Payments.sol/Payments.json" with { type: "json" };
 import TestERC20 from "../artifacts/contracts/TestERC20.sol/TestERC20.json" with { type: "json" };
 import SemaphoreHSS from "../artifacts/contracts/SemaphoreHSS.sol/SemaphoreHSS.json" with { type: "json" };
-import { Wallet, ContractFactory, JsonRpcProvider, Contract } from "ethers";
+import {
+  Wallet,
+  ContractFactory,
+  JsonRpcProvider,
+  Contract,
+  ErrorFragment,
+  AbiCoder,
+  keccak256,
+} from "ethers";
 import { ChannelManager } from "./ChannelManager.ts";
 import * as dotenv from "dotenv";
 import fs from "fs";
@@ -33,6 +41,9 @@ async function main() {
   const operator = Wallet.fromPhrase(process.env.MNEMONIC, rpc);
   const subscriber = Wallet.fromPhrase(process.env.SUBSCRIBER_A, rpc);
   const provider = Wallet.fromPhrase(process.env.PROVIDER_A, rpc);
+  console.log("Operator:", operator.address);
+  console.log("Subscriber:", subscriber.address);
+  console.log("Provider:", provider.address);
 
   // Get TEST token contract. If there is no contract, deploy one.
   const TestERC20Contract = (TestERC20 as any).address
@@ -55,26 +66,26 @@ async function main() {
   // TODO: Ensure subscriber is registered with HSS.
 
   // Make channel manager for our provider to use.
-  const channelManager = new ChannelManager(
-    process.env.MNEMONIC,
-    {
-      [chainId.toString()]: {
-        providers: ["https://rpc2.sepolia.org"],
-        assets: [await TestERC20Contract.getAddress()],
-        contracts: {
-          Payments: {
-            abi: Payments.abi,
-            address: await PaymentsContract.getAddress(),
-          },
-          SemaphoreHSS: {
-            abi: SemaphoreHSS.abi,
-            address: await SemaphoreHSSContract.getAddress(),
-          },
-        },
-      },
-    },
-    10 * 60
-  );
+  // const channelManager = new ChannelManager(
+  //   process.env.MNEMONIC,
+  //   {
+  //     [chainId.toString()]: {
+  //       providers: ["https://rpc2.sepolia.org"],
+  //       assets: [await TestERC20Contract.getAddress()],
+  //       contracts: {
+  //         Payments: {
+  //           abi: Payments.abi,
+  //           address: await PaymentsContract.getAddress(),
+  //         },
+  //         SemaphoreHSS: {
+  //           abi: SemaphoreHSS.abi,
+  //           address: await SemaphoreHSSContract.getAddress(),
+  //         },
+  //       },
+  //     },
+  //   },
+  //   10 * 60
+  // );
 
   // Mint tokens for subscriber.
   const amount = 1000;
@@ -93,7 +104,7 @@ async function main() {
         amount - currentAmount
       );
       const receipt = await res.wait();
-      console.log("Mint res:", res);
+      console.log("Mint tx:", receipt.hash);
 
       // Check new amount to be confirm.
       {
@@ -110,24 +121,58 @@ async function main() {
     const minimumAmount = BigInt("10000000000000000"); // 0.01 ETH
     // Check current balance.
     {
-      const res = await rpc.getBalance(subscriber);
+      const res = await rpc.getBalance(subscriber.address);
       currentAmount = BigInt(res.toString());
       console.log("Subscriber ETH:", currentAmount.toString());
     }
     // Add more ETH if needed.
     if (currentAmount < minimumAmount) {
+      console.log("Funding subscriber some ETH...");
       const res = await operator.sendTransaction({
         to: subscriber.address,
         value: BigInt("50000000000000000") - BigInt(currentAmount.toString()), // 0.05 ETH
       });
       const receipt = await res.wait();
-      console.log("Fund ETH receipt:", receipt);
+      console.log("Fund ETH tx:", receipt.hash);
 
       // Check new balance.
       {
         const res = await rpc.getBalance(subscriber);
         currentAmount = BigInt(res.toString());
         console.log("Subscriber ETH:", currentAmount.toString());
+      }
+    }
+  }
+
+  // Send ETH to provider if needed.
+  {
+    let currentAmount = BigInt("0");
+    const minimumAmount = BigInt("5000000000000000"); // 0.005 ETH
+    // Check current balance.
+    {
+      const res = await rpc.getBalance(provider.address);
+      currentAmount = BigInt(res.toString());
+      console.log("Provider ETH:", currentAmount.toString());
+    }
+    {
+      const res = await rpc.getBalance(operator.address);
+      console.log("Operator ETH:", res);
+    }
+    // Add more ETH if needed.
+    if (currentAmount < minimumAmount) {
+      console.log("Funding provider some ETH...");
+      const res = await operator.sendTransaction({
+        to: provider.address,
+        value: BigInt("10000000000000000") - BigInt(currentAmount.toString()), // 0.01 ETH
+      });
+      const receipt = await res.wait();
+      console.log("Fund ETH tx:", receipt.hash);
+
+      // Check new balance.
+      {
+        const res = await rpc.getBalance(provider);
+        currentAmount = BigInt(res.toString());
+        console.log("Provider ETH:", currentAmount.toString());
       }
     }
   }
@@ -140,25 +185,102 @@ async function main() {
       console.log("Token is not permitted yet on Payments contract.");
       const res = await PaymentsContract.allowAsset(address);
       const receipt = await res.wait();
-      console.log("allowAsset receipt:", receipt);
+      console.log("allowAsset tx:", receipt.hash);
+    } else {
+      console.log("Token is permitted on Payments contract.");
     }
   }
 
   // Subscriber deposits tokens into the Payments contract.
   {
-    console.log("Depositing tokens:", amount);
-    // For some reason ethers' .connect() fn not working.
-    const PaymentsContractSubscriber = new Contract(
-      (Payments as any).address,
-      Payments.abi,
-      subscriber
-    );
-    const res = await PaymentsContractSubscriber.deposit(
-      await TestERC20Contract.getAddress(),
-      amount
-    );
-    const receipt = await res.wait();
-    console.log("Deposit receipt:", receipt);
+    try {
+      // For some reason ethers' .connect() fn not working.
+      const PaymentsContractSubscriber = new Contract(
+        await PaymentsContract.getAddress(),
+        Payments.abi,
+        subscriber
+      );
+      // Get current balance.
+      let currentBalance = BigInt("0");
+      {
+        const res = await PaymentsContractSubscriber.checkBalance(
+          await TestERC20Contract.getAddress()
+        );
+        currentBalance = BigInt(res.toString());
+      }
+      console.log(
+        "Subscriber current Payments contract balance:",
+        currentBalance
+      );
+      if (currentBalance < BigInt(amount.toString())) {
+        // Approve spending for Payments contract.
+        {
+          const TestERC20ContractSubscriber = new Contract(
+            await TestERC20Contract.getAddress(),
+            TestERC20.abi,
+            subscriber
+          );
+          // Get current allowance.
+          let currentAllowance = BigInt("0");
+          {
+            const res = await TestERC20ContractSubscriber.allowance(
+              subscriber.address,
+              await PaymentsContract.getAddress()
+            );
+            currentAllowance = BigInt(res.toString());
+          }
+          console.log(
+            "Payments contract allowance:",
+            currentAllowance.toString()
+          );
+          // If insufficient, approve more token spending.
+          if (currentAllowance < BigInt(amount.toString())) {
+            const res = await TestERC20ContractSubscriber.approve(
+              await PaymentsContract.getAddress(),
+              amount
+            );
+            const receipt = await res.wait();
+            console.log("Approve tx:", receipt.hash);
+            // Double check allowance is correct.
+            {
+              const res = await TestERC20ContractSubscriber.allowance(
+                subscriber.address,
+                await PaymentsContract.getAddress()
+              );
+              currentAllowance = BigInt(res.toString());
+            }
+            console.log(
+              "Payments contract allowance:",
+              currentAllowance.toString()
+            );
+          }
+        }
+        // Deposit tokens.
+        console.log("Depositing tokens:", amount);
+        const res = await PaymentsContractSubscriber.deposit(
+          await TestERC20Contract.getAddress(),
+          amount
+        );
+        const receipt = await res.wait();
+        console.log("Deposit tx:", receipt.hash);
+      }
+    } catch (e) {
+      const selector = e.data.substring(0, 10);
+      const fragment = PaymentsContract.interface.fragments.find(
+        (fragment) => (fragment as ErrorFragment).selector === selector
+      );
+      if (fragment) {
+        const revert = {
+          name: (fragment as ErrorFragment).name,
+          signature: fragment.format(),
+          args: PaymentsContract.interface.decodeErrorResult(
+            fragment as ErrorFragment,
+            e.data
+          ),
+        };
+        console.log(revert);
+      }
+    }
   }
 
   // TODO: Replace with ID.
@@ -174,9 +296,53 @@ async function main() {
   console.log("Got nonce:", nonce);
 
   // Have the subscriber sign off on a payment.
-  const types = ["uint256", "address", "uint256", "bytes"];
-  const values = [nonce, await TestERC20Contract.getAddress(), 100];
-  // const sig = subscriber.signMessage();
+  const types = ["address", "uint256", "address", "uint256"];
+  const values = [
+    provider.address,
+    nonce,
+    await TestERC20Contract.getAddress(),
+    100,
+  ];
+  const encoded = AbiCoder.defaultAbiCoder().encode(types, values);
+  const hash = keccak256(encoded);
+  const sig = await subscriber.signMessage(hash);
+  console.log("Signed:", values);
+  console.log("Signature:", sig);
+
+  // Submit claim using provider.
+  {
+    const PaymentsContractProvider = new Contract(
+      await PaymentsContract.getAddress(),
+      Payments.abi,
+      provider
+    );
+    try {
+      const res = await PaymentsContractProvider.claim(
+        nonce,
+        await TestERC20Contract.getAddress(),
+        100,
+        sig
+      );
+      const receipt = res.wait();
+      console.log("Claim receipt:", receipt);
+    } catch (e) {
+      const selector = e.data.substring(0, 10);
+      const fragment = PaymentsContract.interface.fragments.find(
+        (fragment) => (fragment as ErrorFragment).selector === selector
+      );
+      if (fragment) {
+        const revert = {
+          name: (fragment as ErrorFragment).name,
+          signature: fragment.format(),
+          args: PaymentsContract.interface.decodeErrorResult(
+            fragment as ErrorFragment,
+            e.data
+          ),
+        };
+        console.log(revert);
+      }
+    }
+  }
 }
 
 main();
